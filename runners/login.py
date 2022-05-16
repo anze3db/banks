4a12cc12933d01304a25cb91dfe19d0e2abc7aa3
Anže Pečar <anze@pecar.me>
2022-05-16 14:59:41 +0100

    Login using multiple threads

--- runners/login.py
+++ runners/login.py
@@ -1,15 +1,26 @@
+import asyncio
+from concurrent.futures.thread import ThreadPoolExecutor
+
 from .bankinter import login as bankinter_login
 from .business import login as business_login
 from .n26 import login as n26_login
 
 
 def run(args):
+
+    executor = ThreadPoolExecutor(10)
+
+    loop = asyncio.get_event_loop()
+
     if args.bankinter:
-        bankinter_login()
-        input("Logged in 🔓")
+        loop.run_in_executor(executor, bankinter_login)
     if args.business:
-        business_login()
-        input("Logged in 🔓")
+        loop.run_in_executor(executor, business_login)
     if args.n26:
-        n26_login()
-        input("Logged in 🔓")
+        loop.run_in_executor(executor, n26_login)
+
+    loop.run_until_complete(asyncio.gather(*asyncio.all_tasks(loop)))
+
+    input("Logged in 🔓")
+    loop.close()
+    executor.shutdown()
